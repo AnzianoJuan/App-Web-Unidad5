71415481db8f8ef07153466b1b88fea87e4c3ac2
Juan anziano <juanianziano22@gmail.com>
2025-01-20 17:15:20 -0300

    Seteo del proyecto con sus respectivas clases

---
+++ Dominio/Disco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Dominio
+{
+    public class Disco
+    {
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+ 
+        public int CantidadCanciones { get; set; }
+
+        public string UrlImagenTapa {  get; set; }
+
+
+        public Estilo Estilo { get; set; }
+
+        public Edicion Edicion { get; set; }
+
+    }
+}
